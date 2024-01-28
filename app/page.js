28401faf7007ed8756920876d6b2b18173fb496b
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2024-01-29 01:34:19 +0600

    Refactor layout and add content to home page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
-    <main>
+    <main className="mt-10">
       <div>
-        <h2>Hello World!</h2>
+        <h2>Consequat Lorem dolore nulla consequat veniam enim tempor eu. Eiusmod incididunt et enim dolore ea in ipsum. Et eiusmod nisi do consequat incididunt duis reprehenderit dolor. Veniam irure minim nulla qui est. Do ipsum qui amet nulla excepteur consectetur et sint excepteur reprehenderit qui aliqua commodo. Sunt velit nulla adipisicing pariatur proident enim est dolor elit nulla ea commodo. Dolor quis ad nostrud laboris ad excepteur aute ipsum.</h2>
+
       </div>
     </main>
   );
