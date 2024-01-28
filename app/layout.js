42f88668249bab342675e5b587f676b85175421b
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2024-01-29 01:49:08 +0600

    Add VSCode settings and update navigation in layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter( { subsets: [ "latin" ] } );
 
@@ -12,7 +13,16 @@ export default function RootLayout( { children } ) {
   return (
     <html lang="en" className='p-6'>
       <body className={ inter.className }>
-        <nav> Home | About</nav>
+        <nav>
+          <ul className="flex gap-6">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
         { children }
       </body>
     </html>
