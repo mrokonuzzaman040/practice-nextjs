82499239a00c693701594bb7768cb1f69b820027
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2024-01-29 01:40:20 +0600

    Remove unnecessary line in Home component

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ export default function Home() {
     <main className="mt-10">
       <div>
         <h2>Consequat Lorem dolore nulla consequat veniam enim tempor eu. Eiusmod incididunt et enim dolore ea in ipsum. Et eiusmod nisi do consequat incididunt duis reprehenderit dolor. Veniam irure minim nulla qui est. Do ipsum qui amet nulla excepteur consectetur et sint excepteur reprehenderit qui aliqua commodo. Sunt velit nulla adipisicing pariatur proident enim est dolor elit nulla ea commodo. Dolor quis ad nostrud laboris ad excepteur aute ipsum.</h2>
-
       </div>
     </main>
   );
